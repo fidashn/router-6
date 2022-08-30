0d0ff4fdac1e503d6fd0896a55e3cc0477bb5fdd
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-30 12:43:14 +0300

    installed react router & created pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     hello! 
-    </div>
-  );
+   <Router>
+    {/* <a href='/home'>Go to home page</a> */}
+   <Routes>
+   <Route path=''/>
+   </Routes>
+   </Router>
+   )
 }
 
 export default App;
